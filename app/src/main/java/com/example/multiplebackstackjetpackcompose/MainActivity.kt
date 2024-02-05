cb6785e0939e0148056eea67dae297b85bba58ef
puja110 <csta.puja@gmail.com>
2024-02-04 20:47:05 -0500

    bottom nav bar added

--- app/src/main/java/com/example/multiplebackstackjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/multiplebackstackjetpackcompose/MainActivity.kt
@@ -3,22 +3,95 @@ package com.example.multiplebackstackjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.multiplebackstackjetpackcompose.ui.theme.MultipleBackstackJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MultipleBackstackJetpackComposeTheme {
+                val rootNavController = rememberNavController()
+                val navBackStackEntry by rootNavController.currentBackStackEntryAsState()
+                Scaffold(
+                    bottomBar = {
+                        NavigationBar {
+                            items.forEach { item ->
+                                val isSelected =
+                                    item.title.lowercase() == navBackStackEntry?.destination?.route
+                                NavigationBarItem(
+                                    selected = isSelected,
+                                    label = { Text(text = item.title) },
+                                    icon = {
+                                        Icon(
+                                            imageVector = if (isSelected) {
+                                                item.selectedIcon
+                                            } else {
+                                                item.unselectedIcon
+                                            }, contentDescription = item.title
+                                        )
+                                    },
+                                    onClick = {
+                                        rootNavController.navigate(item.title.lowercase()) {
+                                            popUpTo(rootNavController.graph.findStartDestination().id) {
+                                                saveState = true
+                                            }
+                                            launchSingleTop = true
+                                            restoreState = true
+                                        }
+                                    }
 
+                                )
+                            }
+
+                        }
+                    }
+                ) {
+
+                }
             }
         }
     }
 }
+
+data class BottomNavigationItem(
+    val title: String,
+    val unselectedIcon: ImageVector,
+    val selectedIcon: ImageVector
+)
+
+val items = listOf(
+    BottomNavigationItem(
+        title = "Home",
+        unselectedIcon = Icons.Outlined.Home,
+        selectedIcon = Icons.Filled.Home
+    ),
+    BottomNavigationItem(
+        title = "Chat",
+        unselectedIcon = Icons.Outlined.Email,
+        selectedIcon = Icons.Filled.Email
+    ),
+    BottomNavigationItem(
+        title = "Setings",
+        unselectedIcon = Icons.Outlined.Settings,
+        selectedIcon = Icons.Filled.Settings
+    )
+)
